638a55bbeaa5861133cdc3570cd794a357e5364d
spark <parkyngj@gmail.com>
2020-02-24 17:54:21 -0800

    use cors

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const jsonServer = require('json-server');
+const cors = require('cors');
 const clone = require('clone');
 const data = require('./data.json');
 
 const app = jsonServer.create();
 
-app.all('*', (req, res, next) => {
+app.all('*', cors(), (req, res, next) => {
   if (req.method === 'GET') {
     if (req.path === '/') {
       return res.redirect('https://github.com/sally/adventure-time-api');
