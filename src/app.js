ced069c1731d6e371c21fd5470aaf604ce2daf63
spark <parkyngj@gmail.com>
2020-03-02 14:57:34 -0800

    use json-server middlewares

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const jsonServer = require('json-server');
-const cors = require('cors');
+
+const middlewares = jsonServer.defaults();
+
 const clone = require('clone');
 const data = require('../db/data.json');
 
 const app = jsonServer.create();
 
-app.all('*', cors(), (req, res, next) => {
+app.all('*', (req, res, next) => {
   if (req.method === 'GET') {
     if (req.path === '/') {
       return res.redirect('https://github.com/sally/adventure-time-api');
@@ -19,6 +21,7 @@ app.all('*', cors(), (req, res, next) => {
 const router = jsonServer.router(clone(data));
 router.db._.id = 'slug';
 
+app.use(middlewares);
 app.use('/api/v1', router);
 
 module.exports = app;
